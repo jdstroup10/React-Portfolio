e2638a93884ae64b2f6a1497a1ef77ba59b92539
Jason Strouphauer <jdstroup10@gmail.com>
2020-05-20 18:27:49 -0500

    Updates

---
+++ precache-manifest.96ea0c2158b56f9846bc2ce2c56c75ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac15088499b3fac7084de226e1ef92b6",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "48865efa12670af72fde",
+    "url": "/React-Portfolio/static/css/main.5114bcef.chunk.css"
+  },
+  {
+    "revision": "36168f4441861c8b2975",
+    "url": "/React-Portfolio/static/js/2.6ab2f2c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.6ab2f2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48865efa12670af72fde",
+    "url": "/React-Portfolio/static/js/main.08610fb3.chunk.js"
+  },
+  {
+    "revision": "c3d7c804f21d34cc577a",
+    "url": "/React-Portfolio/static/js/runtime-main.ad880b05.js"
+  },
+  {
+    "revision": "7f8e4ae24f25b91a2b87f17223617879",
+    "url": "/React-Portfolio/static/media/orange_yellow_background_202072.7f8e4ae2.jpg"
+  }
+]);
